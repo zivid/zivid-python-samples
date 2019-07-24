238ae0908062ef5c4f881d0d028146690b652392
AzureAD\SatjaSivcev <satja.sivcev@zivid.com>
2019-07-24 18:28:46 +0200

    Major code fix

--- read_ZDF_vis_3D.py
+++ read_ZDF_vis_3D.py
@@ -7,42 +7,44 @@ Depth map).
 import zivid
 import numpy as np
 import matplotlib.pyplot as plt
-from vtk_visualizer import *
+from vtk_visualizer import plotxyzrgb
+
 
 def _main():
-    
+
     app = zivid.Application()
-    
+
     # The Zivid3D.zdf file has to be in the same folder as this sample script.
-    FilenameZDF = "Zivid3D.zdf"
-    
-    print("Reading ", FilenameZDF, " point cloud" )    
-    frame = zivid.Frame(FilenameZDF)
-    
+    filename_zdf = "Zivid3D.zdf"
+
+    print(f"Reading {filename_zdf} point cloud")
+    frame = zivid.Frame(filename_zdf)
+
+    # Getting the point cloud
     pc = frame.get_point_cloud()
-    xyz = np.dstack([pc['x'],pc['y'],pc['z']])
-    image = np.dstack([pc['r'],pc['g'],pc['b']])
-    
-    pc = np.dstack([xyz,image])
+    xyz = np.dstack([pc["x"], pc["y"], pc["z"]])
+    rgb = np.dstack([pc["r"], pc["g"], pc["b"]])
+    pc = np.dstack([xyz, rgb])
+
+    # Flattening the point cloud
     pts = pc.reshape(-1, 6)
 
-    # Plot them
+    # Display the point cloud
     plotxyzrgb(pts)
-    
-    print("Displaying the RGB image and the Depth map")
-    
-    plt.figure()
-    plt.imshow(image)
-    plt.title('RGB image')
-    
-    plt.figure()
-    plt.imshow(xyz[:,:,2], vmin=np.nanmin(xyz[:,:,2]), vmax=np.nanmax(xyz[:,:,2]), cmap="jet")
-    plt.colorbar()
-    plt.title('Depth map')
-    
-if __name__ == "__main__":
-    _main()
-    
 
-    
-    
+    # Displaying the RGB image
+    plt.figure()
+    plt.imshow(rgb)
+    plt.title("RGB image")
+
+    # Displaying the Depth Image
+    Z = xyz[:, :, 2]
+    plt.figure()
+    plt.imshow(Z, vmin=np.nanmin(Z), vmax=np.nanmax(Z), cmap="jet")
+    plt.colorbar()
+    plt.title("Depth map")
+
+
+if __name__ == "__main__":
+    # Before running this script run '%gui qt'
+    _main()
