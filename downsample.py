ba1f96efe1a0780770b4509fc33355f01333ad5c
AzureAD\SatjaSivcev <satja.sivcev@zivid.com>
2019-07-24 18:53:33 +0200

    init commit

---
+++ downsample.py
@@ -0,0 +1,26 @@
+"""
+Import ZDF point cloud and downsample it.
+"""
+
+import zivid
+import numpy as np
+
+
+def _main():
+
+    app = zivid.Application()
+
+    # The Zivid3D.zdf file has to be in the same folder as this sample script.
+    filename_zdf = "Zivid3D.zdf"
+
+    print(f"Reading {filename_zdf} point cloud")
+    frame = zivid.Frame(filename_zdf)
+
+    # Getting the point cloud
+    pc = frame.get_point_cloud()
+    xyz = np.dstack([pc["x"], pc["y"], pc["z"]])
+    rgb = np.dstack([pc["r"], pc["g"], pc["b"]])
+
+
+if __name__ == "__main__":
+    _main()
