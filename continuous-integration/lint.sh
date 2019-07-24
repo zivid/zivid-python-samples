2262b1854584380610659656a97fac22af451298
Trym Bremnes <trym.bremnes@zivid.com>
2019-07-24 13:32:28 +0200

    Enabled pylint

--- continuous-integration/lint.sh
+++ continuous-integration/lint.sh
@@ -13,4 +13,9 @@ echo Running flake8 on:
 echo "$pythonFiles"
 flake8 --config="$ROOT_DIR/.flake8" $pythonFiles || exit $?
 
+echo Running pylint on:
+echo "$pythonFiles"
+pylint -j 0 --rcfile "$ROOT_DIR/.pylintrc" $pythonFiles || exit $?
+
+
 echo Success! ["$(basename $0)"]
