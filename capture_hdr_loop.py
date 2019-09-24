12e9bdf62eac66570da182b0f58c0e53270f3120
Torbjorn Sorby <torbjorn.sorby@zivid.com>
2019-09-24 10:01:53 +0200

    Modify settings and refactor
    Read settings from .yaml file, and use them in a loop to capture HDR.
    The format in the .yaml file is the same as that which is output from
    Zivid Studio.

--- capture_hdr_loop.py
+++ capture_hdr_loop.py
@@ -1,9 +1,39 @@
-"""
-Capture HDR frames in a loop (while actively changing some HDR settings).
+"""Capture HDR frames in a loop (while actively changing HDR settings).
+
+Three sets of HDR settings are chosen. These settings provide a dynamic range of about 9-10 stops,
+with a decent lowlight and highlight frame, which is sufficient to grab good data on most scenes,
+including shiny, for a medium camera operating at around 0.7 m to 1.2 m.
+
 """
 
 import datetime
+from pathlib import Path
+import yaml
 import zivid
+
+
+def _read_settings_from_file(path: Path) -> zivid.Settings:
+    settings_from_yaml = yaml.load(open(path, "rb").read(), Loader=yaml.Loader)[
+        "Settings"
+    ]
+    settings = zivid.Settings(
+        brightness=settings_from_yaml["Brightness"],
+        exposure_time=datetime.timedelta(
+            microseconds=settings_from_yaml["ExposureTime"]
+        ),
+        gain=settings_from_yaml["Gain"],
+        iris=settings_from_yaml["Iris"],
+    )
+    filters = settings_from_yaml["Filters"]
+    settings.filters.contrast.enabled = filters["Contrast"]["Enabled"]
+    settings.filters.contrast.threshold = filters["Contrast"]["Threshold"]
+    settings.filters.gaussian.enabled = filters["Gaussian"]["Enabled"]
+    settings.filters.gaussian.sigma = filters["Gaussian"]["Sigma"]
+    settings.filters.outlier.enabled = filters["Outlier"]["Enabled"]
+    settings.filters.outlier.threshold = filters["Outlier"]["Threshold"]
+    settings.filters.reflection.enabled = filters["Reflection"]["Enabled"]
+    settings.filters.saturated.enabled = filters["Saturated"]["Enabled"]
+    return settings
 
 
 def _main():
@@ -12,57 +42,26 @@ def _main():
     print("Connecting to the camera")
     camera = app.connect_camera()
 
-    print("Configuring the camera settings")
-    settings_collection = [camera.settings for _ in range(3)]
+    # Get current settings, to be modified before use
+    settings = [camera.settings for _ in range(3)]
 
-    settings_collection[0].brightness = 1.8
-    settings_collection[0].gain = 1
-    settings_collection[0].filters.gaussian.enabled = True
-    settings_collection[0].filters.gaussian.sigma = 1.5
-    settings_collection[0].filters.reflection.enabled = True
+    for hdr_index in range(3):
+        print(f"Capturing an HDR image, alternative settings #{hdr_index+1}")
+        for frame_index in range(len(settings)):
+            settings[frame_index] = _read_settings_from_file(
+                Path(f"settings/set{hdr_index+1}/frame_0{frame_index+1}.yml")
+            )
+            print(
+                f"\tFrame {frame_index + 1}:"
+                f" Iris: {settings[frame_index].iris}"
+                f" Exposure: {settings[frame_index].exposure_time.microseconds / 1000}ms"
+                f" Gain: {settings[frame_index].gain}"
+            )
 
-    settings_collection[1].brightness = 1.8
-    settings_collection[1].gain = 1
-    settings_collection[1].filters.gaussian.enabled = True
-    settings_collection[1].filters.gaussian.sigma = 1.5
-    settings_collection[1].filters.reflection.enabled = True
-
-    settings_collection[2].brightness = 1.8
-    settings_collection[2].gain = 1
-    settings_collection[2].filters.gaussian.enabled = True
-    settings_collection[2].filters.gaussian.sigma = 1.5
-    settings_collection[2].filters.reflection.enabled = True
-
-    exposure_time_frame_1 = [
-        datetime.timedelta(microseconds=10000),
-        datetime.timedelta(microseconds=90000),
-        datetime.timedelta(microseconds=40000),
-    ]
-    exposure_time_frame_2 = [
-        datetime.timedelta(microseconds=40000),
-        datetime.timedelta(microseconds=10000),
-        datetime.timedelta(microseconds=90000),
-    ]
-    exposure_time_frame_3 = [
-        datetime.timedelta(microseconds=90000),
-        datetime.timedelta(microseconds=40000),
-        datetime.timedelta(microseconds=10000),
-    ]
-    iris_frames = [10, 20, 30]
-
-    for i in range(len(iris_frames)):
-        settings_collection[0].exposure_time = exposure_time_frame_1[i]
-        settings_collection[1].exposure_time = exposure_time_frame_2[i]
-        settings_collection[2].exposure_time = exposure_time_frame_3[i]
-
-        settings_collection[0].iris = iris_frames[i]
-        settings_collection[1].iris = iris_frames[i]
-        settings_collection[2].iris = iris_frames[i]
-
-        print("Capturing an HDR frame")
-        with camera.capture(settings_collection) as hdr_frame:
-            print("Saving the HDR frame")
-            hdr_frame.save("HDR.zdf")
+        with camera.capture(settings) as hdr_frame:
+            out_file_name = f"HDR_Settings_{hdr_index+1}.zdf"
+            print(f"Saving the HDR frame to: {out_file_name}")
+            hdr_frame.save(out_file_name)
 
 
 if __name__ == "__main__":
