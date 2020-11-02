ebruun/python-samples

Capture and Settings API updates
capture_hdr_separate_frames.py is removed because the API to combine
frames is removed. HDR capture is always taken care of by the capture
API. An HDR happens if more than one acquisition setting is provided
in the settings argument.

source/applications/basic/capture_hdr_loop.py
@@ -6,34 +6,11 @@ including shiny, for a medium camera operating at around 0.7 m to 1.2 m.
 
 """
 
-import datetime
 from pathlib import Path
-import yaml
 import zivid
 
 from utils.paths import get_sample_data_path
-
-
-def _read_settings_from_file(path: Path) -> zivid.Settings:
-    settings_from_yaml = yaml.load(path.read_text(), Loader=yaml.Loader)["Settings"]
-    settings = zivid.Settings(
-        brightness=settings_from_yaml["Brightness"],
-        exposure_time=datetime.timedelta(
-            microseconds=settings_from_yaml["ExposureTime"]
-        ),
-        gain=settings_from_yaml["Gain"],
-        iris=settings_from_yaml["Iris"],
-    )
-    filters = settings_from_yaml["Filters"]
-    settings.filters.contrast.enabled = filters["Contrast"]["Enabled"]
-    settings.filters.contrast.threshold = filters["Contrast"]["Threshold"]
-    settings.filters.gaussian.enabled = filters["Gaussian"]["Enabled"]
-    settings.filters.gaussian.sigma = filters["Gaussian"]["Sigma"]
-    settings.filters.outlier.enabled = filters["Outlier"]["Enabled"]
-    settings.filters.outlier.threshold = filters["Outlier"]["Threshold"]
-    settings.filters.reflection.enabled = filters["Reflection"]["Enabled"]
-    settings.filters.saturated.enabled = filters["Saturated"]["Enabled"]
-    return settings
+from utils.settings_from_file import get_settings_from_yaml
 
 
 def _main():
@@ -42,29 +19,14 @@ def _main():
     print("Connecting to the camera")
     camera = app.connect_camera()
 
-    # Initialize settings list
-    number_of_frames_per_hdr = 3
+    for hdr_index in range(1, 4):
+        print(f"Capturing an HDR image, alternative settings #{hdr_index}")
+        settings = get_settings_from_yaml(
+            Path() / get_sample_data_path() / f"Settings/Settings{hdr_index:02d}.yml"
+        )
 
-    for hdr_index in range(3):
-        print(f"Capturing an HDR image, alternative settings #{hdr_index+1}")
-        settingslist = []
-        for frame_index in range(number_of_frames_per_hdr):
-            settings = _read_settings_from_file(
-                Path()
-                / Path()
-                / get_sample_data_path()
-                / "Settings/Set{hdr_index+1}/Frame0{frame_index+1}.yml"
-            )
-            print(
-                f"\tFrame {frame_index + 1}:"
-                f" Iris: {settings.iris}"
-                f" Exposure: {settings.exposure_time.microseconds / 1000}ms"
-                f" Gain: {settings.gain}"
-            )
-            settingslist.append(settings)
-
-        with zivid.hdr.capture(camera, settingslist) as hdr_frame:
-            out_file_name = f"HDR_Settings_{hdr_index+1}.zdf"
+        with camera.capture(settings) as hdr_frame:
+            out_file_name = f"HDR_Settings_{hdr_index}.zdf"
             print(f"Saving the HDR frame to: {out_file_name}")
             hdr_frame.save(out_file_name)
 

zivid/zivid-python-samples

source/applications/basic/capture_hdr_loop.py
@@ -6,32 +6,11 @@ including shiny, for a medium camera operating at around 0.7 m to 1.2 m.
 
 """
 
-import datetime
 from pathlib import Path
-import yaml
 import zivid
 
 from sample_utils.paths import get_sample_data_path
-
-
-def _read_settings_from_file(path: Path) -> zivid.Settings:
-    settings_from_yaml = yaml.load(path.read_text(), Loader=yaml.Loader)["Settings"]
-    settings = zivid.Settings(
-        brightness=settings_from_yaml["Brightness"],
-        exposure_time=datetime.timedelta(microseconds=settings_from_yaml["ExposureTime"]),
-        gain=settings_from_yaml["Gain"],
-        iris=settings_from_yaml["Iris"],
-    )
-    filters = settings_from_yaml["Filters"]
-    settings.filters.contrast.enabled = filters["Contrast"]["Enabled"]
-    settings.filters.contrast.threshold = filters["Contrast"]["Threshold"]
-    settings.filters.gaussian.enabled = filters["Gaussian"]["Enabled"]
-    settings.filters.gaussian.sigma = filters["Gaussian"]["Sigma"]
-    settings.filters.outlier.enabled = filters["Outlier"]["Enabled"]
-    settings.filters.outlier.threshold = filters["Outlier"]["Threshold"]
-    settings.filters.reflection.enabled = filters["Reflection"]["Enabled"]
-    settings.filters.saturated.enabled = filters["Saturated"]["Enabled"]
-    return settings
+from sample_utils.settings_from_file import get_settings_from_yaml
 
 
 def _main():
@@ -40,26 +19,12 @@ def _main():
     print("Connecting to the camera")
     camera = app.connect_camera()
 
-    # Initialize settings list
-    number_of_frames_per_hdr = 3
+    for hdr_index in range(1, 4):
+        print(f"Capturing an HDR image, alternative settings #{hdr_index}")
+        settings = get_settings_from_yaml(Path() / get_sample_data_path() / f"Settings/Settings{hdr_index:02d}.yml")
 
-    for hdr_index in range(3):
-        print(f"Capturing an HDR image, alternative settings #{hdr_index+1}")
-        settingslist = []
-        for frame_index in range(number_of_frames_per_hdr):
-            settings = _read_settings_from_file(
-                Path() / Path() / get_sample_data_path() / "Settings/Set{hdr_index+1}/Frame0{frame_index+1}.yml"
-            )
-            print(
-                f"\tFrame {frame_index + 1}:"
-                f" Iris: {settings.iris}"
-                f" Exposure: {settings.exposure_time.microseconds / 1000}ms"
-                f" Gain: {settings.gain}"
-            )
-            settingslist.append(settings)
-
-        with zivid.hdr.capture(camera, settingslist) as hdr_frame:
-            out_file_name = f"HDR_Settings_{hdr_index+1}.zdf"
+        with camera.capture(settings) as hdr_frame:
+            out_file_name = f"HDR_Settings_{hdr_index}.zdf"
             print(f"Saving the HDR frame to: {out_file_name}")
             hdr_frame.save(out_file_name)
 