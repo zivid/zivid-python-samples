7d61d80236cdfb2bcb96d05399477370d3827e3c
Mathias Nedrebø <mathias.nedrebo@zividlabs.com>
2023-05-03 16:09:18 +0200

    Removed redundant `$?` for `exit` statements in shell scipts.
    The `exit` statement itself inherits the return status of the current
    expression, hence all the `$?` can be removed without altering error
    handling.

--- continuous-integration/setup.sh
+++ continuous-integration/setup.sh
@@ -7,19 +7,19 @@ ZIVID_SDK_EXACT_VERSION=2.9.0+4dbba385-1
 ZIVID_TELICAM_EXACT_VERSION=3.0.1.1-3
 
 export DEBIAN_FRONTEND=noninteractive
-source /etc/os-release || exit $?
+source /etc/os-release || exit
 
 function apt-yes {
     apt-get --assume-yes "$@"
 }
 
 function install_www_deb {
-    TMP_DIR=$(mktemp --tmpdir --directory zivid-python-install-www-deb-XXXX) || exit $?
-    pushd $TMP_DIR || exit $?
-    wget -nv "$@" || exit $?
-    apt-yes install --fix-broken ./*deb || exit $?
-    popd || exit $?
-    rm -r $TMP_DIR || exit $?
+    TMP_DIR=$(mktemp --tmpdir --directory zivid-python-install-www-deb-XXXX) || exit
+    pushd $TMP_DIR || exit
+    wget -nv "$@" || exit
+    apt-yes install --fix-broken ./*deb || exit
+    popd || exit
+    rm -r $TMP_DIR || exit
 }
 
 apt-yes update || exit
@@ -28,10 +28,10 @@ apt-yes dist-upgrade || exit
 apt-yes install \
     python3-pip \
     wget ||
-    exit $?
+    exit
 
-install_www_deb "https://downloads.zivid.com/sdk/releases/${ZIVID_SDK_EXACT_VERSION}/u${VERSION_ID:0:2}/zivid-telicam-driver_${ZIVID_TELICAM_EXACT_VERSION}_amd64.deb" || exit $?
-install_www_deb "https://downloads.zivid.com/sdk/releases/${ZIVID_SDK_EXACT_VERSION}/u${VERSION_ID:0:2}/zivid_${ZIVID_SDK_EXACT_VERSION}_amd64.deb" || exit $?
+install_www_deb "https://downloads.zivid.com/sdk/releases/${ZIVID_SDK_EXACT_VERSION}/u${VERSION_ID:0:2}/zivid-telicam-driver_${ZIVID_TELICAM_EXACT_VERSION}_amd64.deb" || exit
+install_www_deb "https://downloads.zivid.com/sdk/releases/${ZIVID_SDK_EXACT_VERSION}/u${VERSION_ID:0:2}/zivid_${ZIVID_SDK_EXACT_VERSION}_amd64.deb" || exit
 
 python3 -m pip install --upgrade pip || exit
 python3 -m pip install --requirement "$ROOT_DIR/requirements.txt" || exit
